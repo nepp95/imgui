a87a006e6b8080b39864e9b2d0d717153033ff09
Niels Eppenhof <n.eppenhof@gmail.com>
2025-06-08 02:26:33 +0200

    Update

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "imgui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "Off"
+
+    targetdir ("%{wks.location}/Bin/" .. OutputDir .. "/%{prj.name}")
+    objdir ("%{wks.location}/Bin-Int/" .. OutputDir .. "/%{prj.name}")
+
+    defines {
+        "IMGUI_IMPL_VULKAN_USE_VOLK"
+    }
+
+    files {
+        "*.cpp",
+        "*.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_vulkan.cpp",
+        "backends/imgui_impl_vulkan.h",
+        "misc/cpp/imgui_stdlib.cpp",
+        "misc/cpp/imgui_stdlib.h",
+        "misc/debuggers/imgui.natvis",
+        "misc/debuggers/imgui.natstepfilter",
+    }
+
+    includedirs {
+        "%{IncludeDir.glfw}",
+        "%{IncludeDir.imgui}",
+        "%{IncludeDir.volk}",
+        "%{IncludeDir.vulkan}"
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "On"
